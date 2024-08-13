3d37c7aec01ee69708ce3c3fcce6751c378e4a10
Shaheer Amjad <155566482+shaheeramjad@users.noreply.github.com>
2024-08-14 00:37:47 +0500

    Update ProjectsCard.js

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -25,7 +25,7 @@ const ProjectsCard = ({ title, des, src }) => {
               rel="noreferrer"
               className="bannerIcon"
             >
-              <FaGithub />
+              <BsGithub />
             </a>
               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
                 <FaGlobe />
