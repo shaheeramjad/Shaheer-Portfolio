9271cf48e97d6e109ab71579ecdc7b0bfe332cc5
Shaheer Amjad <shaheeransari450@gmail.com>
2025-09-09 15:26:34 +0500

    New

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BsGithub } from "react-icons/bs";
-import { FaGlobe } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 const ProjectsCard = ({ title, des, src, projectId }) => {
